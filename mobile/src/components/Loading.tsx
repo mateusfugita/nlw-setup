5eeefa5ebf91848e9d84d526d44a7603e41d6b34
Mateus Fugita <mateushiwao@gmail.com>
2023-01-18 19:42:22 -0300

    feat: create basic structure for mobile project

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import { View, ActivityIndicator } from 'react-native'
+
+export function Loading(){
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#09090A' }}>
+      <ActivityIndicator color='#7C3AED' />
+    </View>
+  )
+}
