0f9b4ee5d47cb8f85f4913d5defa944aee72b83c
Mateus Fugita <mateushiwao@gmail.com>
2023-01-21 19:43:30 -0300

    feat: save check option when completing an habit

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,22 +1,27 @@
+import { useState } from 'react';
 import * as Popover from '@radix-ui/react-popover';
-import * as Checkbox from '@radix-ui/react-checkbox';
+import dayjs from 'dayjs';
 import clsx from 'clsx';
 
 import { ProgressBar } from './ProgressBar';
-import { Check } from 'phosphor-react';
-import dayjs from 'dayjs';
+import { HabitsList } from './HabitsList';
 
 interface HabitDayProps{
   date: Date
-  completed?: number
+  defaultCompleted?: number
   amount?: number
 }
 
-export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps){
+export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayProps){
+  const [completed, setCompleted] = useState(defaultCompleted);
   const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
 
   const dayAndMonth = dayjs(date).format('DD/MM')
   const dayOfWeek = dayjs(date).format('dddd')
+
+  function handleCompletedChange(completed: number){
+    setCompleted(completed)
+  }
 
   return (
     <Popover.Root>
@@ -42,21 +47,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps){
 
           <ProgressBar progress={completedPercentage} />
 
-          <div className='mt-6 flex flex-col gap-3'>
-            <Checkbox.Root
-              className='flex items-center gap-3 group'
-            >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
-                <Checkbox.Indicator>
-                  <Check size={20} className='text-white'/>
-                </Checkbox.Indicator>
-              </div>
-
-              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitsList date={date} onCompletedChange={handleCompletedChange} />
 
           <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
         </Popover.Content>
---
+++ web/src/components/HabitsList.tsx
@@ -0,0 +1,81 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
+import dayjs from "dayjs";
+import { Check } from "phosphor-react";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
+
+interface HabitsListProps{
+  date: Date;
+  onCompletedChange: (completed: number) => void;
+}
+
+interface HabitsInfo{
+  possibleHabits: Array<{
+    id: string;
+    title: string;
+    created_at: string;
+  }>,
+  completedHabits: string[],
+}
+
+export function HabitsList({ date, onCompletedChange }: HabitsListProps){
+  const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
+
+  useEffect(() => {
+    api.get('/day', {
+      params: {
+        date: date.toISOString(),
+      }
+    }).then(response => {
+      setHabitsInfo(response.data)
+    })
+  }, [])
+
+  async function handleToggleHabit(habitId: string){
+    await api.patch(`/habits/${habitId}/toggle`)
+
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
+    let completedHabits: string[] = []
+
+    if(isHabitAlreadyCompleted){
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
+    }
+    else{
+      completedHabits = [...habitsInfo!.completedHabits, habitId]
+    }
+
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits
+    })
+
+    onCompletedChange(completedHabits.length)
+  }
+
+  const isDateinPast = dayjs(date).endOf('day').isBefore(new Date())
+
+  return (
+    <div className='mt-6 flex flex-col gap-3'>
+      {habitsInfo?.possibleHabits.map(habit => (
+        <Checkbox.Root
+          key={habit.id}
+          onCheckedChange={() => handleToggleHabit(habit.id)}
+          className='flex items-center gap-3 group'
+          checked={habitsInfo.completedHabits.includes(habit.id)}
+          disabled={isDateinPast}
+        >
+          <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+            <Checkbox.Indicator>
+              <Check size={20} className='text-white'/>
+            </Checkbox.Indicator>
+          </div>
+
+          <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+            {habit.title}
+          </span>
+        </Checkbox.Root>
+      ))}
+      
+    </div>
+  )
+}
